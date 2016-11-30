938422eb55c346984ed72ce9c9188b111ba54bc4
Ashley Schuett <ashley@containership.io>
2016-11-30 16:00:41 -0500

    core reference refactor (#35)
    * core reference refactor
    
    * changed routes.js to use one line per route
    
    * deleted authed middleware
    
    * updated cluster state get call, added 500 erros to get applications
    
    * changed myraid errors to return 500 instead of 400
    
    * changed errors to return new Error()

--- handlers/v1/host.js
+++ handlers/v1/host.js
@@ -3,78 +3,54 @@
 const _ = require('lodash');
 const async = require('async');
 
-module.exports = function(core) {
+module.exports = {
+    // get host
+    get(req, res, next) {
+        const core = req.core;
+        const hosts = _.indexBy(core.cluster.legiond.get_peers(), 'id');
 
-    return {
-        // get host
-        get(req, res, next) {
-            const hosts = _.indexBy(core.cluster.legiond.get_peers(), 'id');
+        const attributes = core.cluster.legiond.get_attributes();
+        hosts[attributes.id] = attributes;
 
-            const attributes = core.cluster.legiond.get_attributes();
-            hosts[attributes.id] = attributes;
+        const host = hosts[req.params.host];
+        if(_.isUndefined(host)) {
+            return next();
+        }
+        host.containers = [];
 
-            const host = hosts[req.params.host];
-            if(_.isUndefined(host)) {
+        return core.cluster.myriad.persistence.keys([core.constants.myriad.CONTAINERS_PREFIX, '*', '*'].join(core.constants.myriad.DELIMITER), (err, containers) => {
+            if(err) {
+                res.stash.code = 500;
                 return next();
             }
-            host.containers = [];
 
-            return core.cluster.myriad.persistence.keys([core.constants.myriad.CONTAINERS_PREFIX, '*', '*'].join(core.constants.myriad.DELIMITER), (err, containers) => {
-                if(err) {
-                    res.stash.code = 400;
-                    return next();
-                }
-
-                return async.each(containers, (container_name, fn) => {
-                    return core.cluster.myriad.persistence.get(container_name, (err, container) => {
-                        if(err) {
-                            return fn();
-                        }
-
-                        try {
-                            container = JSON.parse(container);
-                            if(container.host == host.id) {
-                                container.application = container_name.split(core.constants.myriad.DELIMITER)[2];
-                                host.containers.push(container);
-                            }
-                        } catch(err) { /* do nothing */ }
+            return async.each(containers, (container_name, fn) => {
+                return core.cluster.myriad.persistence.get(container_name, (err, container) => {
+                    if(err) {
                         return fn();
-                    });
-                }, () => {
-                    res.stash.code = 200;
-                    res.stash.body = host;
-                    return next();
+                    }
+
+                    try {
+                        container = JSON.parse(container);
+                        if(container.host == host.id) {
+                            container.application = container_name.split(core.constants.myriad.DELIMITER)[2];
+                            host.containers.push(container);
+                        }
+                    } catch(err) { /* do nothing */ }
+                    return fn();
                 });
+            }, () => {
+                res.stash.code = 200;
+                res.stash.body = host;
+                return next();
             });
-        },
+        });
+    },
 
         // update host
-        update(req, res, next) {
-            if(!_.isUndefined(req.body) && _.has(req.body, 'tags')) {
-                const hosts = _.indexBy(core.cluster.legiond.get_peers(), 'id');
-
-                const attributes = core.cluster.legiond.get_attributes();
-                hosts[attributes.id] = attributes;
-
-                const host = hosts[req.params.host];
-
-                if(!_.isUndefined(host)) {
-                    core.cluster.legiond.send({
-                        event: core.constants.events.UPDATE_HOST,
-                        data: req.body.tags
-                    }, host);
-
-                    res.stash.code = 200;
-                }
-            } else {
-                res.stash.code = 400;
-            }
-
-            return next();
-        },
-
-        // delete host
-        delete(req, res, next) {
+    update(req, res, next) {
+        const core = req.core;
+        if(!_.isUndefined(req.body) && _.has(req.body, 'tags')) {
             const hosts = _.indexBy(core.cluster.legiond.get_peers(), 'id');
 
             const attributes = core.cluster.legiond.get_attributes();
@@ -84,14 +60,38 @@ module.exports = function(core) {
 
             if(!_.isUndefined(host)) {
                 core.cluster.legiond.send({
-                    event: core.constants.events.DELETE_HOST
+                    event: core.constants.events.UPDATE_HOST,
+                    data: req.body.tags
                 }, host);
 
-                res.stash.code = 204;
+                res.stash.code = 200;
             }
-
-            return next();
+        } else {
+            res.stash.code = 400;
         }
-    };
+
+        return next();
+    },
+
+    // delete host
+    delete(req, res, next) {
+        const core = req.core;
+        const hosts = _.indexBy(core.cluster.legiond.get_peers(), 'id');
+
+        const attributes = core.cluster.legiond.get_attributes();
+        hosts[attributes.id] = attributes;
+
+        const host = hosts[req.params.host];
+
+        if(!_.isUndefined(host)) {
+            core.cluster.legiond.send({
+                event: core.constants.events.DELETE_HOST
+            }, host);
+
+            res.stash.code = 204;
+        }
+
+        return next();
+    }
 
 };
--- handlers/v1/variable.js
+++ handlers/v1/variable.js
@@ -2,113 +2,113 @@
 
 const _ = require('lodash');
 
-module.exports = function(core) {
-
-    return {
-        // get variable
-        get(req, res, next) {
-            return core.cluster.myriad.persistence.get([core.constants.myriad.VARIABLES_PREFIX, req.params.variable].join(core.constants.myriad.DELIMITER), (err, value) => {
-                if(err && err.name == core.constants.myriad.ENOKEY) {
-                    res.stash.code = 404;
-                } else if(err) {
-                    res.stash.code = 400;
-                } else {
-                    res.stash = {
-                        code: 200,
-                        body: value
-                    };
-                }
-
-                return next();
-            });
-        },
-
-        // create variable
-        create(req, res, next) {
-            if(!_.has(req.body, 'value')) {
+module.exports = {
+    // get variable
+    get(req, res, next) {
+        const core = req.core;
+        return core.cluster.myriad.persistence.get([core.constants.myriad.VARIABLES_PREFIX, req.params.variable].join(core.constants.myriad.DELIMITER), (err, value) => {
+            if(err && err.name == core.constants.myriad.ENOKEY) {
+                res.stash.code = 404;
+            } else if(err) {
+                res.stash.code = 500;
+            } else {
                 res.stash = {
-                    code: 400,
-                    body: {
-                        error: 'Missing \'value\' field!'
-                    }
+                    code: 200,
+                    body: value
                 };
-
-                return next();
             }
 
-            return core.cluster.myriad.persistence.get([core.constants.myriad.VARIABLES_PREFIX, req.params.variable].join(core.constants.myriad.DELIMITER), (err/*, value*/) => {
-                if(err && err.name == core.constants.myriad.ENOKEY) {
-                    return core.cluster.myriad.persistence.set([core.constants.myriad.VARIABLES_PREFIX, req.params.variable].join(core.constants.myriad.DELIMITER), req.body.value, (err) => {
-                        if(err) {
-                            res.stash.code = 400;
-                        } else {
-                            res.stash.code = 201;
-                        }
-                        return next();
-                    });
-                } else if(err) {
-                    res.stash.code = 400;
-                    return next();
-                } else {
-                    res.stash = {
-                        code: 400,
-                        body: {
-                            error: 'Variable already exists!'
-                        }
-                    };
+            return next();
+        });
+    },
 
-                    return next();
+    // create variable
+    create(req, res, next) {
+        const core = req.core;
+        if(!_.has(req.body, 'value')) {
+            res.stash = {
+                code: 400,
+                body: {
+                    error: 'Missing \'value\' field!'
                 }
-            });
-        },
+            };
 
-        // update variable
-        update(req, res, next) {
-            if(!_.has(req.body, 'value')) {
-                res.stash = {
-                    code: 400,
-                    body: {
-                        error: 'Missing \'value\' field!'
-                    }
-                };
+            return next();
+        }
 
-                return next();
-            }
-
-            return core.cluster.myriad.persistence.get([core.constants.myriad.VARIABLES_PREFIX, req.params.variable].join(core.constants.myriad.DELIMITER), (err/*, value*/) => {
-                if(err && err.name == core.constants.myriad.ENOKEY) {
-                    res.stash.code = 404;
-                    return next();
-                } else if(err) {
-                    res.stash.code = 400;
-                    return next();
-                }
-
+        return core.cluster.myriad.persistence.get([core.constants.myriad.VARIABLES_PREFIX, req.params.variable].join(core.constants.myriad.DELIMITER), (err/*, value*/) => {
+            if(err && err.name == core.constants.myriad.ENOKEY) {
                 return core.cluster.myriad.persistence.set([core.constants.myriad.VARIABLES_PREFIX, req.params.variable].join(core.constants.myriad.DELIMITER), req.body.value, (err) => {
                     if(err) {
-                        res.stash.code = 400;
+                        res.stash.code = 500;
                     } else {
-                        res.stash.code = 200;
+                        res.stash.code = 201;
                     }
                     return next();
                 });
+            } else if(err) {
+                res.stash.code = 500;
+                return next();
+            } else {
+                res.stash = {
+                    code: 400,
+                    body: {
+                        error: 'Variable already exists!'
+                    }
+                };
 
-            });
-        },
+                return next();
+            }
+        });
+    },
 
-        // delete variable
-        delete(req, res, next) {
-            return core.cluster.myriad.persistence.delete([core.constants.myriad.VARIABLES_PREFIX, req.params.variable].join(core.constants.myriad.DELIMITER), (err/*, value*/) => {
-                if(err && err.name == core.constants.myriad.ENOKEY) {
-                    res.stash.code = 404;
-                } else if(err) {
-                    res.stash.code = 400;
+    // update variable
+    update(req, res, next) {
+        const core = req.core;
+        if(!_.has(req.body, 'value')) {
+            res.stash = {
+                code: 400,
+                body: {
+                    error: 'Missing \'value\' field!'
+                }
+            };
+
+            return next();
+        }
+
+        return core.cluster.myriad.persistence.get([core.constants.myriad.VARIABLES_PREFIX, req.params.variable].join(core.constants.myriad.DELIMITER), (err/*, value*/) => {
+            if(err && err.name == core.constants.myriad.ENOKEY) {
+                res.stash.code = 404;
+                return next();
+            } else if(err) {
+                res.stash.code = 500;
+                return next();
+            }
+
+            return core.cluster.myriad.persistence.set([core.constants.myriad.VARIABLES_PREFIX, req.params.variable].join(core.constants.myriad.DELIMITER), req.body.value, (err) => {
+                if(err) {
+                    res.stash.code = 500;
                 } else {
-                    res.stash.code = 204;
+                    res.stash.code = 200;
                 }
                 return next();
             });
-        }
-    };
 
+        });
+    },
+
+    // delete variable
+    delete(req, res, next) {
+        const core = req.core;
+        return core.cluster.myriad.persistence.delete([core.constants.myriad.VARIABLES_PREFIX, req.params.variable].join(core.constants.myriad.DELIMITER), (err/*, value*/) => {
+            if(err && err.name == core.constants.myriad.ENOKEY) {
+                res.stash.code = 404;
+            } else if(err) {
+                res.stash.code = 500;
+            } else {
+                res.stash.code = 204;
+            }
+            return next();
+        });
+    }
 };
