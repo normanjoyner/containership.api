bf786d2684c1e300ba1f58df7f6f91a3e8271602
Ashley <ashley@containership.io>
2017-02-01 14:25:16 -0500

    updated max_consecutive_failures to unhealthy_threshold

--- handlers/v1/application.js
+++ handlers/v1/application.js
@@ -115,11 +115,11 @@ module.exports = {
             if(_.has(req.body, 'health_checks')) {
                 body.health_checks = req.body.health_checks;
                 const health_check_defaults = {
-                    interval : 30000,
-                    healthy_threshold : 1,
-                    max_consecutive_failures : 3,
-                    timeout : 5000,
-                    type : 'tcp'
+                    interval: 30000,
+                    healthy_threshold: 1,
+                    unhealthy_threshold: 3,
+                    timeout: 5000,
+                    type: 'tcp'
                 };
 
                 _.each(body.health_checks, (health_check) => {
